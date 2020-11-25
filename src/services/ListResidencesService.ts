134bb8a83ef66056ba684fcba481d075aae9ac90
Kauã Steliano <stelianok@gmail.com>
2020-11-25 14:51:36 -0300

    Fixed filter

--- src/services/ListResidencesService.ts
+++ src/services/ListResidencesService.ts
@@ -17,10 +17,17 @@ class ListResidencesService {
   }: Request): Promise<Residence[]> {
     const residenceRepository = getRepository(Residence);
 
-    const residences = await residenceRepository.query(
-      'SELECT residences.*, users.name, users.email, users.avatar,users.bio, users.phone, users.is_host, users.user_city, users.user_state, users.is_email_available, users.is_phone_available, users.is_location_available FROM residences  INNER JOIN users ON residences.owner_id = users.id WHERE (residences.available = true) AND ((CAST($1 AS FLOAT)) IS NULL OR (CAST(price as FLOAT)) <= (CAST($1 AS FLOAT))) AND (CAST($2 AS TEXT)  IS NULL OR residences.residence_place = $2) AND CAST($3 AS TEXT) IS NULL OR residences.city = $3 ORDER BY residences.id',
-      [price, residence_place, city],
+    let residences = await residenceRepository.query(
+      'SELECT residences.*, users.name, users.email, users.avatar,users.bio, users.phone, users.is_host, users.user_city, users.user_state, users.is_email_available, users.is_phone_available, users.is_location_available FROM residences  INNER JOIN users ON residences.owner_id = users.id WHERE (residences.available = true) AND (CAST($1 AS TEXT)  IS NULL OR residences.residence_place = $1) AND CAST($2 AS TEXT) IS NULL OR residences.city = $2 ORDER BY residences.id',
+      [residence_place, city],
     );
+
+    if (price) {
+      residences = residences.filter(
+        (residence: Residence) => residence.price <= price,
+      );
+    }
+
     return residences;
   }
 }
